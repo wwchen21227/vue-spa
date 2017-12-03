57e32af3463ba3df8942a93427cb8facbd363c61
Chen Weng Wei <wwchen21227@gmail.com>
2017-12-03 16:14:36 +0800

    initial commit

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Category from './theme/Category.vue'
+import Login from './theme/Login.vue'
+import NotFound from './theme/NotFound.vue'
+
+// const Category = () => System.import('./theme/Category.vue')
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: (to, from, savedPosition) => ({ y: 0 }),
+  routes: [
+    {path: '/Login', component: Login},
+    {path: '/category/:id', name: 'category', component: Category},
+    {path: '/', redirect: '/category/front-end'},
+    {path: '*', component: NotFound}
+  ]
+})
+
+export default router
---
+++ src/theme/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <router-link to="/" exact>
+        <img src="http://bit.ly/vue-img"
+          alt="Vue SPA" />
+      </router-link>
+      <router-link to="/category/front-end" class="nav-item is-tab">
+        Front-end
+      </router-link>
+      <router-link :to="{ name: 'category', params: { id:'mobile' } }" class="nav-item is-tab">
+        Mobile
+      </router-link>
+      <router-link to="/Login" class="nav-item is-tab">
+        <span v-if="isAuthenticated">Logout</span>
+        <span v-else>Login</span>
+      </router-link>
+    </div>
+  </nav>
+</template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters(['isAuthenticated'])
+    }
+  }
+</script>
